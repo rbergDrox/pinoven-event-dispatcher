5b8bd5f25fe00e7b31cdd6e8e5c03ae493e87e28
Raphaël BERGINA <bergina.raphael@gmail.com>
2020-06-29 23:16:33 -0300

    Prioritize.php improve cyclomatic complexity

--- src/Priority/Prioritize.php
+++ src/Priority/Prioritize.php
@@ -28,6 +28,26 @@ class Prioritize implements PrioritizeInterface
     }
 
     /**
+     * Determine the position between 2 items.
+     * @param $itemPriority1
+     * @param $itemPriority2
+     * @return int
+     */
+    private function determineRankPriority($itemPriority1, $itemPriority2)
+    {
+        $rank = 0;
+        if ($itemPriority1 instanceof ItemPriorityInterface
+            && $itemPriority2 instanceof ItemPriorityInterface) {
+            $rank = $itemPriority1->getPriority() < $itemPriority2->getPriority() ? 1: -1;
+        } elseif ($itemPriority1 instanceof ItemPriorityInterface) {
+            $rank = $itemPriority1->getPriority() < 0 ? 1: -1;
+        } elseif ($itemPriority2 instanceof ItemPriorityInterface) {
+            $rank = 0 < $itemPriority2->getPriority() ? 1: -1;
+        }
+        return $rank;
+    }
+
+    /**
      * @inheritDoc
      */
     public function sortItems(iterable $items): iterable
@@ -35,21 +55,10 @@ class Prioritize implements PrioritizeInterface
         if (!is_array($items) && $items instanceof Traversable) {
             $items = $this->traversableToArray($items);
         }
-
         usort(
             $items,
             function ($itemPriority1, $itemPriority2) {
-                if ($itemPriority1 instanceof ItemPriorityInterface
-                    && $itemPriority2 instanceof ItemPriorityInterface) {
-                    return $itemPriority1->getPriority() < $itemPriority2->getPriority() ? 1: -1;
-                }
-                if ($itemPriority1 instanceof ItemPriorityInterface) {
-                    return $itemPriority1->getPriority() < 0 ? 1: -1;
-                }
-                if ($itemPriority2 instanceof ItemPriorityInterface) {
-                    return 0 < $itemPriority2->getPriority() ? 1: -1;
-                }
-                return 0;
+                return $this->determineRankPriority($itemPriority1, $itemPriority2);
             }
         );
 
